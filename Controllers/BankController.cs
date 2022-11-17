92047f3c26712f48cf4f5a97650c0dfb269e72df
nish159 <77575013+nish159@users.noreply.github.com>
2022-11-17 10:38:40 -0500

    Add files via upload

---
+++ Controllers/BankController.cs
@@ -0,0 +1,39 @@
+﻿using Bank;
+using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System.Collections.Generic;
+
+namespace OnlineBankingApiService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly ILogger<BankController> _logger;
+
+        public BankController(IAccountRepository accountRepository, ILogger<BankController> logger)
+        {
+            _accountRepository = accountRepository;
+            _logger = logger;
+        }
+
+        [HttpGet("/accounts", Name = nameof(GetAllAccountsAsync))]
+        public async Task<IActionResult> GetAllAccountsAsync()
+        {
+            Result<List<Account>> getAllResult = await _accountRepository.GetAllAccountsAsync();
+            if(getAllResult.Succeeded == false)
+            {
+                return BadRequest();
+            }
+            return Ok(getAllResult.Value);
+        }
+
+        [HttpGet("/number2", Name = nameof(GetNumber2))]
+        public IActionResult GetNumber2()
+        {
+            return Ok(5);
+        }
+    }
+}
