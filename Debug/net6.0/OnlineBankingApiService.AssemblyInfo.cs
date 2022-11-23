78acbae08c49c6904ee4aa864ebcddc165b4c53f
nish159 <77575013+nish159@users.noreply.github.com>
2022-11-23 18:35:43 -0500

    Update OnlineBankingApiService.AssemblyInfo.cs

--- Debug/net6.0/OnlineBankingApiService.AssemblyInfo.cs
+++ Debug/net6.0/OnlineBankingApiService.AssemblyInfo.cs
@@ -11,9 +11,6 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBankingApiService")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBankingApiService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBankingApiService")]
