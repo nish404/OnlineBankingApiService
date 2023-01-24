31522c28053b9c2e3c67950893d743eb380123d6
nish159 <77575013+nish159@users.noreply.github.com>
2023-01-24 10:11:54 -0500

    Add files via upload

---
+++ OnlineBankingApiService.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Data;
+using DataAccess;
+using Logic;
+
+namespace OnlineBankingApiService
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance.
+    /// </summary>
+    internal sealed class OnlineBankingApiService : StatelessService
+    {
+        public OnlineBankingApiService(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
+                    {
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+
+                        var builder = WebApplication.CreateBuilder();
+
+                        builder.Services.AddSingleton<StatelessServiceContext>(serviceContext);
+                        builder.WebHost
+                                    .UseKestrel()
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                    .UseUrls(url);
+                        
+                        // Add services to the container.
+                        
+                        builder.Services.AddControllers();
+                        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                        builder.Services.AddEndpointsApiExplorer();
+                        builder.Services.AddSwaggerGen();
+
+                        // Add repositories
+                        builder.Services.AddSingleton<IAccountRepository, AccountsCosmosDbRepository>();
+                        builder.Services.AddSingleton<IUserRepository, UsersCosmosDbRepository>();
+                        
+                        // Add logic
+                        builder.Services.AddSingleton<IAccountLogic, AccountLogic>();
+
+                        var app = builder.Build();
+                        
+                        // Configure the HTTP request pipeline.
+                        if (app.Environment.IsDevelopment())
+                        {
+                            app.UseSwagger();
+                            app.UseSwaggerUI();
+                        }
+                        
+                        app.UseAuthorization();
+                        
+                        app.MapControllers();
+                        
+                        return app;
+                    }))
+            };
+        }
+    }
+}
